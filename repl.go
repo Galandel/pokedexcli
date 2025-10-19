ec08508fc539b8739b3c6da451c2009c07229be0
Galandel <17754014+Galandel@users.noreply.github.com>
2025-10-18 18:58:50 -0600

    Set up commands exit and help

--- repl.go
+++ repl.go
@@ -8,31 +8,58 @@ import (
 )
 
 func startRepl() {
-	scanner := bufio.NewScanner(os.Stdin)
+	reader := bufio.NewScanner(os.Stdin)
+
 	for {
 		// Print the prompt
 		fmt.Print("Pokedex > ")
-
-		// Wait for user input
-		if !scanner.Scan() {
-			// Exit the loop if there's an error or EOF
-			break
-		}
+		reader.Scan()
 
 		// Get the user's input and clean it
-		input := cleanInput(scanner.Text())
+		input := cleanInput(reader.Text())
 
 		if len(input) == 0 {
-			// fmt.Println()
 			continue
 		}
 
-		// Print the first word
-		fmt.Printf("Your command was: %s\n", input[0])
+		commandName := input[0]
+
+		// check for command item
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
 }
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
-	// return make([]string, 0)
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
